5c7175699fada676d8117b5372556f5b859f9ef0
Jeff Triplett <jeff.triplett@gmail.com>
2010-01-06 22:49:07 -0600

    created a json version as the main app and the old version is represented as reditty_web

--- reditty.py
+++ reditty.py
@@ -5,64 +5,66 @@ Based off ``redbottle`` (http://github.com/tnm/redbottle/)
 
 """
 __author__ = 'Jeff Triplett'
-__version__ = ('0', '1', '0')
+__version__ = ('0', '1', '1')
 
 import redis
 from itty import get, post, run_itty, Response
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
 
 r = redis.Redis()
 
-def template(file, vars={}, **args):
-    """
-    Super simple template language...
-
-    """
-    contents = open(file, 'r').read()
-    return str(contents % (dict(vars, **args)))
-
-
-def keys_template(all_keys):
-    temp = []
-    for keys in all_keys:
-        temp.append('<p><a href="/keyvalue/show/%(keys)s">%(keys)s</p>' % ({'keys': keys}))
-    return '\n'.join(temp)
-
-
-@get('/keyvalue/')
-def template_keyvalue(request):
-    all_keys = r.keys('*')
-    values = dict(title='Key-Value Store', all_keys=keys_template(all_keys))
-    return Response(template('templates/keys.html', values))
-
 
 @post('/keyvalue/add/')
-def template_add(request):
+def keyvalue_add(request):
     key = request.POST.get('key')
     value = request.POST.get('value')
+    success = False
+
     if key and value:
         r.set(key, value)
-    all_keys = r.keys('*')
-    values = dict(title="Key-Value Pair", key=key, value=value, all_keys=keys_template(all_keys))
-    return Response(template('templates/keys.html', values))
+        success = True
+
+    return Response(json.dumps({'status': success}), content_type='application/json')
 
 
 @post('/keyvalue/delete/')
-def template_delete(request):
-    key_delete = request.POST.get('key_delete')
-    if key_delete:
-        r.delete(key_delete)
-    all_keys = r.keys('*')
-    values = dict(title="Key-Value Pair", key=key_delete, all_keys=all_keys)
-    return Response(template('templates/delete.html', values))
+def keyvalue_delete(request):
+    success = False
+
+    if 'key' in request.POST:
+        key = request.POST.get('key')
+        key = key.strip()
+        value = r.get(key)
+
+        if value:
+            r.delete(key)
+            success = True
+
+    return Response(json.dumps({'status': success}), content_type='application/json')
 
 
-@get('/keyvalue/show/(?P<key>\w+)/')
-def template_show(request, key):
-    the_key = key.strip()
-    value = r.get(the_key)
-    values = dict(title=the_key, the_key=the_key, value=value)
-    return Response(template('templates/show.html', values))
+@get('/keyvalue/(?P<key>\w+)/')
+def keyvalue_display(request, key):
+    key = key.strip()
+    value = r.get(key)
+    success = False
+
+    if not value:
+        return Response(json.dumps({'status': success}), content_type='application/json')
+
+    return Response(json.dumps({'key': key, 'value': value}), content_type='application/json')
+
+
+@get('/keyvalue/')
+def keyvalue_list(request):
+    keys = r.keys('*')
+
+    return Response(json.dumps({'keys': keys}), content_type='application/json')
 
 
 if __name__ == '__main__':
